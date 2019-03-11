48b6d58c491d339240b7a6828d436495fb2ceb73
jchou21 <jchou21@dtechhs.org>
2019-03-11 10:02:30 -0700

    finished buildPyrimid

--- Pyramid.java
+++ Pyramid.java
@@ -26,9 +26,10 @@ public class Pyramid extends GraphicsProgram {
 	
 	public void run() {
 		/* You fill this in. */
-		drawRow(14,1);
-		drawRow(13,2);
+//		drawRow(14,1);
+//		drawRow(13,2);
 		//drawBrick(0,-1*BRICK_HEIGHT + getHeight(),BRICK_WIDTH,BRICK_HEIGHT);
+		buildPyrimid(14);
 	}
 
 	public void drawBrick(int x, int y, int width, int height) {
@@ -36,12 +37,22 @@ public class Pyramid extends GraphicsProgram {
 		add(rect);
 	}
 	public void drawRow(int brickNum, int layer){
-		if(layer%2 == 1) {
-			int layerHeight = getHeight() - layer * BRICK_HEIGHT;
+		int layerHeight = getHeight() - layer * BRICK_HEIGHT;
+		if(brickNum%2 == 1) {
 			for (int i = -brickNum / 2; i < brickNum / 2; i++) {
 				System.out.println("i: " + i);
 				drawBrick(getWidth() / 2 + i * BRICK_WIDTH, layerHeight, BRICK_WIDTH, BRICK_HEIGHT);
 			}
+		}else if(brickNum%2 == 0){
+			for (int i = -brickNum/2 + 1;i < brickNum/2;i++){
+				drawBrick(getWidth()/2 - BRICK_WIDTH/2 + i * BRICK_WIDTH,layerHeight,BRICK_WIDTH,BRICK_HEIGHT);
+			}
+		}
+	}
+	public void buildPyrimid(int baseNum){
+		int layers = baseNum;
+		for(int layer = 1; layer < layers; layer++){
+			drawRow(baseNum - layer + 1,layer);
 		}
 	}
 }
