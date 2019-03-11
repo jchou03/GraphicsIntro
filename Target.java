e33a88986c1fcce66c5da6ca7e8de0f820f62c00
jchou21 <jchou21@dtechhs.org>
2019-03-11 10:10:09 -0700

    finished creating target

--- Target.java
+++ Target.java
@@ -14,7 +14,19 @@ public class Target extends GraphicsProgram {
 	private static final int HEIGHT = 270;
 	public void run() {
 		/* You fill this in. */
-		GOval oval = new GOval(0,0, 10, 10);
+
+		GOval oval = new GOval(getWidth()/2 - 36,getHeight()/2 - 36, 72, 72);
+		oval.setFilled(true);
+		oval.setColor(Color.red);
 		add(oval);
+		GOval oval1 = new GOval(getWidth()/2 - 23.4,getHeight()/2 - 23.4,46.8,46.8);
+		oval1.setFilled(true);
+		oval1.setColor(Color.white);
+		add(oval1);
+		//21.6
+		GOval oval2 = new GOval(getWidth()/2 - 10.8,getHeight()/2-10.8,21.6,21.6);
+		oval2.setColor(Color.red);
+		oval2.setFilled(true);
+		add(oval2);
 	}
 }
